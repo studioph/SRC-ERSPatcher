07a55ce8d37996aea1843246897e3c7b915fc0f7
Paul Hutchings <dev@studiop.page>
2024-10-27 00:26:46 -0400

    Initial addon implementation

--- SRCPatcher/Program.cs
+++ SRCPatcher/Program.cs
@@ -2,6 +2,7 @@ using Mutagen.Bethesda;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
+using Noggog;
 using Synthesis.Util;
 using Synthesis.Util.Quest;
 
@@ -9,11 +10,20 @@ namespace SRCPatcher
 {
     public class Program
     {
-        private static readonly ModKey SRC_ERS = ModKey.FromNameAndExtension("SRC_ERS.esp");
+        private static Lazy<QuestPatcherPipeline> _pluginPipeline = null!;
 
-        private static readonly IFormLinkGetter<IFormListGetter> ERS_FormList = FormKey
-            .Factory("000800:SRC_ERS.esp")
-            .ToLinkGetter<IFormListGetter>();
+        static Program()
+        {
+            PluginLoader<ISkyrimMod, ISkyrimModGetter>.Register(
+                (state, mod) => new MissivesPlugin(mod, state, _pluginPipeline.Value)
+            );
+            PluginLoader<ISkyrimMod, ISkyrimModGetter>.Register(
+                (state, mod) => new NoticeBoardPlugin(mod, state, _pluginPipeline.Value)
+            );
+            PluginLoader<ISkyrimMod, ISkyrimModGetter>.Register(
+                (state, mod) => new BountyHunterPlugin(mod, state, _pluginPipeline.Value)
+            );
+        }
 
         public static async Task<int> Main(string[] args)
         {
@@ -22,39 +32,53 @@ namespace SRCPatcher
                 .SetTypicalOpen(GameRelease.SkyrimSE, "SRCPatcher.esp")
                 .AddRunnabilityCheck(state =>
                 {
-                    state.LoadOrder.AssertListsMod(SRC_ERS, $"Missing {SRC_ERS}");
+                    state.LoadOrder.AssertListsMod(SRC_ERS.ModKey, $"Missing {SRC_ERS.ModKey}");
                 })
                 .Run(args);
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            var ersMod = state.LoadOrder.GetIfEnabledAndExists(SRC_ERS);
-            var ersFormList = ERS_FormList.Resolve(state.LinkCache).ToLinkGetter();
+            _pluginPipeline = new Lazy<QuestPatcherPipeline>(
+                () => new QuestPatcherPipeline(state.PatchMod)
+            );
 
-            var affectedQuests = ersMod.Quests;
+            var ersMod = state.LoadOrder.GetIfEnabledAndExists(SRC_ERS.ModKey);
+            var loadedPlugins = PluginLoader<ISkyrimMod, ISkyrimModGetter>.Scan(state);
+            var patcher = new QuestAliasConditionForwarder(SRC_ERS.Condition);
+            var pipeline = new QuestForwarderPipeline(state.PatchMod);
 
-            var condition =
-                QuestAliasUtil.FindAliasCondition(
-                    affectedQuests,
-                    condition =>
-                        condition.Data.Function == Condition.Function.GetInCurrentLocFormList
-                        && (
-                            (IGetInCurrentLocFormListConditionDataGetter)condition.Data
-                        ).FormList.Link.Equals(ersFormList)
-                ) ?? throw new Exception("Unable to find ERS condition in quest aliases, aborting");
-
-            var pipeline = new QuestPatcherPipeline(state.PatchMod);
-            var patcher = new QuestAliasConditionPatcher(condition);
-
-            var forwardContexts = affectedQuests.Select(quest =>
+            var forwardContexts = ersMod.Quests.Select(quest =>
                 quest.WithContext<ISkyrimMod, ISkyrimModGetter, IQuest, IQuestGetter>(
                     state.LinkCache
                 )
             );
+            pipeline.Run(patcher, forwardContexts);
 
-            var patchData = pipeline.GetRecordsToPatch(patcher, forwardContexts);
-            pipeline.PatchRecords(patcher, patchData);
+            foreach (var plugin in loadedPlugins)
+            {
+                plugin.Run();
+            }
+        }
+    }
+
+    public static class SRC_ERS
+    {
+        public static readonly ModKey ModKey = ModKey.FromNameAndExtension("SRC_ERS.esp");
+
+        public static readonly IFormLinkGetter<IFormListGetter> FormList = FormKey
+            .Factory($"000800:{ModKey}")
+            .ToLinkGetter<IFormListGetter>();
+
+        public static readonly IConditionGetter Condition = BuildCondition(FormList);
+
+        private static IConditionGetter BuildCondition(IFormLinkGetter<IFormListGetter> formLink)
+        {
+            IConditionFloat condition = new ConditionFloat();
+            IGetInCurrentLocFormListConditionData data = new GetInCurrentLocFormListConditionData();
+            data.FormList.Link.SetTo(formLink);
+            condition.Data = (ConditionData)data;
+            return condition;
         }
     }
 }
