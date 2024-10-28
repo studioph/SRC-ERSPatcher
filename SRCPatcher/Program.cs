a4d9246f1eb9f77ffca60b6c0774a9bcb95e2bc6
Paul Hutchings <dev@studiop.page>
2024-10-27 21:29:11 -0400

    Update main to reflect new plugin architecture

--- SRCPatcher/Program.cs
+++ SRCPatcher/Program.cs
@@ -10,18 +10,30 @@ namespace SRCPatcher
 {
     public class Program
     {
+        /// <summary>
+        /// Patcher pipeline for plugin instances. Lazy so that it doesn't get created if no plugins are loaded
+        /// </summary>
         private static Lazy<QuestPatcherPipeline> _pluginPipeline = null!;
+
+        /// <summary>
+        /// Loader instance to register plugins with
+        /// </summary>
         private static readonly PluginLoader<ISkyrimMod, ISkyrimModGetter> _loader = new();
 
+        /// <summary>
+        /// Registers the optional ERS patcher plugins with the loader
+        /// </summary>
         static Program()
         {
-            _loader.Register((state, mod) => new MissivesPlugin(mod, state, _pluginPipeline.Value));
-            _loader.Register(
-                (state, mod) => new NoticeBoardPlugin(mod, state, _pluginPipeline.Value)
-            );
-            _loader.Register(
-                (state, mod) => new BountyHunterPlugin(mod, state, _pluginPipeline.Value)
-            );
+            _loader.Register<MissivesPlugin>(mod => new MissivesPlugin(mod, _pluginPipeline.Value));
+            _loader.Register<NoticeBoardPlugin>(mod => new NoticeBoardPlugin(
+                mod,
+                _pluginPipeline.Value
+            ));
+            _loader.Register<BountyHunterPlugin>(mod => new BountyHunterPlugin(
+                mod,
+                _pluginPipeline.Value
+            ));
         }
 
         public static async Task<int> Main(string[] args)
@@ -38,15 +50,19 @@ namespace SRCPatcher
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
+            // Add factory for lazy pipeline creation
+            // Can't be done before here since it relies on patch mod
             _pluginPipeline = new Lazy<QuestPatcherPipeline>(
                 () => new QuestPatcherPipeline(state.PatchMod)
             );
 
+            // Load ERS mod and any optional plugins in user's load order
             var ersMod = state.LoadOrder.GetIfEnabledAndExists(SRC_ERS.ModKey);
-            var loadedPlugins = _loader.Scan(state);
+            var loadedPlugins = _loader.Scan(state.LoadOrder);
             var patcher = new QuestAliasConditionForwarder(SRC_ERS.Condition);
             var pipeline = new QuestForwarderPipeline(state.PatchMod);
 
+            // Patch base ERS mod
             var forwardContexts = ersMod.Quests.Select(quest =>
                 quest.WithContext<ISkyrimMod, ISkyrimModGetter, IQuest, IQuestGetter>(
                     state.LinkCache
@@ -54,28 +70,43 @@ namespace SRCPatcher
             );
             pipeline.Run(patcher, forwardContexts);
 
+            // Run any optional patcher plugins based on user's load order
             foreach (var plugin in loadedPlugins)
             {
-                plugin.Run();
+                plugin.Run(state);
             }
         }
     }
 
+    /// <summary>
+    /// Contains random information about the ERS mod that can be defined ahead of time
+    /// </summary>
     public static class SRC_ERS
     {
         public static readonly ModKey ModKey = ModKey.FromNameAndExtension("SRC_ERS.esp");
 
+        /// <summary>
+        /// The SRC formlist that locations get added to when cleared.
+        /// </summary>
         public static readonly IFormLinkGetter<IFormListGetter> FormList = FormKey
             .Factory($"000800:{ModKey}")
             .ToLinkGetter<IFormListGetter>();
 
-        public static readonly IConditionGetter Condition = BuildCondition(FormList);
+        /// <summary>
+        /// The ERS formlist condition for Location quest aliases. This is what ensures cleared locations are excluded from quests.
+        /// </summary>
+        public static readonly IConditionGetter Condition = BuildCondition();
 
-        private static IConditionGetter BuildCondition(IFormLinkGetter<IFormListGetter> formLink)
+        /// <summary>
+        /// Creates a GetInCurrentLocFormList condition object referencing the SRC formlist.
+        /// The condition isn't complex so it can be statically created up-front to avoid searching for it in the ERS mod at runtime.
+        /// </summary>
+        /// <returns>A condition object that references the SRC formlist</returns>
+        private static IConditionGetter BuildCondition()
         {
             IConditionFloat condition = new ConditionFloat();
             IGetInCurrentLocFormListConditionData data = new GetInCurrentLocFormListConditionData();
-            data.FormList.Link.SetTo(formLink);
+            data.FormList.Link.SetTo(FormList);
             condition.Data = (ConditionData)data;
             return condition;
         }
